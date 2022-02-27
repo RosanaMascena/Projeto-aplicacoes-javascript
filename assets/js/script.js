bc153e61e82c2da2ca0be3106245c2576f8fb34f
Rosana Mascena <rosanamascena@gmail.com>
2022-02-26 23:58:28 -0300

    Botão de pause
    Aparecer botão pause

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,6 @@ document.querySelector('.botao-play').addEventListener('click', tocarMusica);
 
 function tocarMusica(){
     musica.play();
+    document.querySelector('.botao-pause').style.display = 'block';
 }
+
