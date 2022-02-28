07e74aee63937aaa6cae27c384d599b4ae863e9f
Rosana Mascena <rosanamascena@gmail.com>
2022-02-28 18:41:44 -0300

    Eventos e funções
    Terminada a função de play e adicionado o evento de pause e a função de pause.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
 let musica = document.querySelector('audio');
 
+// Eventos
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
 
+document.querySelector('.botao-pause').addEventListener('click', pausarMusica)
+
+// Funcções
 function tocarMusica(){
     musica.play();
     document.querySelector('div .botao-pause').style.display = 'block';
+    musica.play();
+    document.querySelector('.botao-play').style.display = 'none';
 }
+
+function pausarMusica(){
+    musica.pause();
+    document.querySelector('div .botao-pause').style.display = 'none';
+    musica.pause();
+    document.querySelector('.botao-play').style.display = 'block';
+}
+
+
 
