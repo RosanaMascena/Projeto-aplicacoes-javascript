4cf180ff31d51706f3efacc8bd9b4e5a124205b8
Rosana Mascena <83788768+RosanaMascena@users.noreply.github.com>
2022-03-16 20:37:03 -0300

    Atualização do código
    Atualização do código

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,31 @@
-//Início - Player de Música
 let musicas = [
     {titulo:'This Little Light of Mine', artista:'Es Jammy Jams', src:'..\music\This Little Light of Mine - Es Jammy Jams.mp3', img:'..\img\jazz-blues.jpg'},
     {titulo:'Everything Has a Beginning', artista:'Joel Cummins', src:'..\music\Everything Has a Beginning - Joel Cummins.mp3', img:'..\img\cinema.jpg'},
     {titulo:'Communicator', artista:'Reed Mathis', src:'..\music\Communicator - Reed Mathis.mp3', img:'..\img\country.jpg'},
 ];
 
-let musica = document.querySelector('.audio-player');
+let musica = document.querySelector('audio');
 let indexMusica = 0;
 renderizarmusica(indexMusica);
 
-let duracaoMusica = document.querySelector('.fim-player');
-let imagem = document.querySelector('.img-player');
-let nomeMusica = document.querySelector('.descricao-player h2');
-let nomeArtista = document.querySelector('.descricao-player i');
+let duracaoMusica = document.querySelector('.fim');
+let imagem = document.querySelector('img');
+let nomeMusica = document.querySelector('.descricao h2');
+let nomeArtista = document.querySelector('.descricao i');
 
 // Eventos
-document.querySelector('.botao-play-player').addEventListener('click', tocarMusica);
+document.querySelector('.botao-play').addEventListener('click', tocarMusica);
 
-document.querySelector('div .botao-pause-player').addEventListener('click', pausarMusica);
+document.querySelector('div .botao-pause').addEventListener('click', pausarMusica);
 
 musica.addEventListener('timeupdate', atualizarBarra);
 
-document.querySelector('.anterior-player').addEventListener('click', () => {
+document.querySelector('.anterior').addEventListener('click', () => {
     indexMusica--;
     renderizarMusica(indexMusica);
 });
 
-document.querySelector('.proxima-player').addEventListener('click', () => {
+document.querySelector('.proxima').addEventListener('click', () => {
     indexMusica++;
     renderizarMusica(indexMusica);
 });
@@ -44,20 +43,20 @@ function renderizarMusica(index){
 
 function tocarMusica(){
     musica.play();
-    document.querySelector('div .botao-pause-player').style.display = 'block';
-    document.querySelector('.botao-play-player').style.display = 'none';
+    document.querySelector('div .botao-pause').style.display = 'block';
+    document.querySelector('.botao-play').style.display = 'none';
 }
 
 function pausarMusica(){
     musica.pause();
-    document.querySelector('div .botao-pause-player').style.display = 'none';
-    document.querySelector('.botao-play-player').style.display = 'block';
+    document.querySelector('div .botao-pause').style.display = 'none';
+    document.querySelector('.botao-play').style.display = 'block';
 }
 
 function atualizarBarra(){
     let barra = document.querySelector('progress');
     barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
-    let tempoDecorrido = document.querySelector('.inicio-player');
+    let tempoDecorrido = document.querySelector('.inicio');
     tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime)); 
 }
 
